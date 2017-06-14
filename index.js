8a477b72e3d9923bdd0f06deff83c8f1e381de8c
Thomas Kaio <thomaskaio@hotmail.com>
2017-06-13 20:14:43 -0500

    ss

--- index.js
+++ index.js
@@ -3,9 +3,42 @@ var url = require('url');
 var final = [];
   console.log("Started...")
     var server = http.createServer(function (req, res) {
-      
+        // request handling logic...
+        console.log("Some conection happened");
+       var url_parts = url.parse(req.url, true);
+       var c = url_parts.query.iso
+
+       if (!isNaN(c)){
+        var theDate = new Date(c * 1000);
+        var dateString = theDate.toGMTString();
+        if(dateString == "Invalid Date"){
+          varHuman = null;
+          var varUnix = null;
+        }
+        else{
+          varUnix = c;
+          varHuman = dateString;
+        }
+
+      }
+      else{
+        var date = new Date(c).getTime()/1000;
+        if(isNaN(date)){
+          varHuman = null;
+          varUnix = null;
+        }
+        else{
+          var varHuman = new Date(c);
+          varHuman = varHuman.toGMTString();;
+          varUnix = date;
+        }
+      }
+
+        final = {human: varHuman, unix: varUnix};
+
+        res.write(JSON.stringify(final));
         res.end();
 
 
     })
-    server.listen(80);
+    server.listen(8080);
